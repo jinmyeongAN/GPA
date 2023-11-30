b5eb0c06c6a9511304842591953f20a637f40c79
jinmyeongAN <jinmyeong.an@gmail.com>
2023-11-30 20:04:48 +0900

    feat    : build rounterChains

--- packages/rag-chroma/rag_chroma/main.py
+++ packages/rag-chroma/rag_chroma/main.py
@@ -1,9 +1,17 @@
 import os
 
+from langchain.agents.agent_toolkits import create_retriever_tool
 from langchain.chat_models import ChatOpenAI
 from src.config.const import OPENAI_API_KEY
-from src.utils.utils import get_docsSplitter, get_pdfLoader, get_prompt, get_RAG, get_vectorstorce
-from langchain.agents.agent_toolkits import create_retriever_tool
+from src.utils.utils import (
+    get_docsSplitter,
+    get_pdfLoader,
+    get_prompt,
+    get_RAG,
+    get_routerChain,
+    get_vectorstorce,
+    prompt_infos,
+)
 
 os.environ["OPENAI_API_KEY"] = OPENAI_API_KEY
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
@@ -25,15 +33,6 @@ vectorstore = get_vectorstorce(splits)
 # 4. Retrieve
 retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 6})
 
-tool = create_retriever_tool(
-    retriever,
-    "search_state_of_union",
-    "Searches and returns documents regarding the state-of-the-union.",
-)
-tools = [tool]
-
-
-
 # 5. Generate
 llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0)
 prompt = get_prompt(mode="cs")
@@ -45,6 +44,7 @@ answer = get_RAG(
     question="Can you make ten True or False Quiz using the important information in context? you should give me the answer of each Quiz.",
 )
 
-print(answer)
+# Routing
+chain = get_routerChain(llm=llm, prompt_infos=prompt_infos)
 
-# 6. Agent
+# chain.run(PDF_files)
