e3309e65d55d4d890a9175da369713a494ffc174
seokhoon <sh.kang@postech.ac.kr>
2023-12-07 16:49:37 +0900

    Update pdf_loader.py
    Optimize equation segmentation and cleanup code

--- web/utils/pdf_loader.py
+++ web/utils/pdf_loader.py
@@ -1,13 +1,14 @@
-import warnings
-import numpy as np
 import re
+import warnings
 from io import BytesIO
-from langchain.document_loaders import PDFPlumberLoader
-from langchain.document_loaders.parsers import PDFPlumberParser
 from typing import List, Optional, Mapping, Any, Iterator
-from langchain.docstore.document import Document
-from langchain.document_loaders.blob_loaders import Blob
+
+import numpy as np
 import pdfplumber.page
+from langchain.docstore.document import Document
+from langchain.document_loaders import PDFPlumberLoader
+from langchain.document_loaders.blob_loaders import Blob
+from langchain.document_loaders.parsers import PDFPlumberParser
 
 _PDF_FILTER_WITH_LOSS = ["DCTDecode", "DCT", "JPXDecode"]
 _PDF_FILTER_WITHOUT_LOSS = [
@@ -38,6 +39,7 @@ class PDFPlumberLoaderPlus(PDFPlumberLoader):
         )
         blob = Blob.from_path(self.file_path)
         return parser.parse(blob)
+
 
 class PDFPlumberParserPlus(PDFPlumberParser):
     """Parse `PDF` with `PDFPlumber`."""
@@ -117,13 +119,13 @@ class PDFPlumberParserPlus(PDFPlumberParser):
                  or (word['x0'] - indent_size <= sent_x0 <= word['x0'] + indent_size
                      and word_x1 > page_width * 0.75
                      and word['size'] - 0.1 <= font_size <= word['size'] + 0.1))
-                and not bullet_point):  # if word is next part of current text
+                    and not bullet_point):  # if word is next part of current text
                 if word['x0'] <= word_x1 + text_kwargs['gap_tolerance']:
                     sent_str += ' '
                 if is_bold is False and 'Bold' in word['fontname']:
                     is_bold = True
                     sent_str += '*'
-                if sent_str[-1] not in  [' ', '\t'] and word['text'] not in  [' ', '\t']:
+                if sent_str[-1] not in [' ', '\t'] and word['text'] not in [' ', '\t']:
                     sent_str = sent_str + ' ' + word['text']
                 else:
                     sent_str = sent_str + word['text']
@@ -135,7 +137,8 @@ class PDFPlumberParserPlus(PDFPlumberParser):
             else:  # if word is first word of next sentence
                 if is_bold:
                     sent_str += '*'
-                sentence_list.append(re.sub(r' +', ' ', re.sub(r'( *\t+ *)+', '\t', sent_str.rstrip().replace('\xa0', ' '))))
+                sentence_list.append(
+                    re.sub(r' +', ' ', re.sub(r'( *\t+ *)+', '\t', sent_str.rstrip().replace('\xa0', ' '))))
                 # sentence_list.append(re.sub(r'[ \t]+', '\t', sent_str.rstrip().replace('\xa0', ' ')))
                 # sentence_list.append(sent_str.rstrip().replace('\xa0', ' '))
                 if is_title:
@@ -174,14 +177,14 @@ class PDFPlumberParserPlus(PDFPlumberParser):
                 sentence_list.append(page_data)
         return '\n'.join(sentence_list)
 
-
     def _process_page_content(self, page: pdfplumber.page.Page) -> str:
         """Process the page content based on dedupe."""
         if self.dedupe:
             word_list = page.dedupe_chars().extract_words(x_tolerance=3,
                                                           y_tolerance=10,
                                                           keep_blank_chars=True,
-                                                          extra_attrs=['fontname', 'size', 'stroking_color', 'non_stroking_color'])
+                                                          extra_attrs=['fontname', 'size', 'stroking_color',
+                                                                       'non_stroking_color'])
         else:
             word_list = page.extract_words(x_tolerance=3,
                                            y_tolerance=10,
@@ -193,7 +196,7 @@ class PDFPlumberParserPlus(PDFPlumberParser):
         for image_word in image_word_list:
             for word_idx in range(len(word_list)):
                 word_avg_y = (word_list[word_idx]['top'] + word_list[word_idx]['bottom']) / 2
-                if (word_avg_y > image_word['top'] and word_list[word_idx]['x0'] > image_word['x1'])\
+                if (word_avg_y > image_word['top'] and word_list[word_idx]['x0'] > image_word['x1']) \
                         or word_avg_y > image_word['bottom']:
                     word_list.insert(word_idx, image_word)
                     break
@@ -213,7 +216,10 @@ class PDFPlumberParserPlus(PDFPlumberParser):
         for img in page.images:
             if img["stream"]["Filter"].name in _PDF_FILTER_WITHOUT_LOSS:
                 images.append(
-                    [Image.fromarray(np.frombuffer(img["stream"].get_data(), dtype=np.uint8).reshape(img["stream"]["Height"], img["stream"]["Width"], -1)).convert('RGB'), img]
+                    [Image.fromarray(
+                        np.frombuffer(img["stream"].get_data(), dtype=np.uint8).reshape(img["stream"]["Height"],
+                                                                                        img["stream"]["Width"],
+                                                                                        -1)).convert('RGB'), img]
                 )
             elif img["stream"]["Filter"].name in _PDF_FILTER_WITH_LOSS:
                 images.append([Image.open(BytesIO(img["stream"].get_data())).convert('RGB'), img])
@@ -223,7 +229,7 @@ class PDFPlumberParserPlus(PDFPlumberParser):
 
         filted_images = []
         for image_tuple in images:
-            # normalize image background color to white
+            # check if image is mono-color
             bg_color = image_tuple[0].getpixel((0, image_tuple[0].height - 1))
             pixels = image_tuple[0].load()
             bg_ratio = 0
@@ -233,8 +239,8 @@ class PDFPlumberParserPlus(PDFPlumberParser):
                 for j in range(image_tuple[0].size[0]):
                     # if pixels[j, i] == bg_color:
                     if (bg_color[0] - 20 <= pixels[j, i][0] <= bg_color[0] + 20
-                        and bg_color[1] - 20 <= pixels[j, i][1] <= bg_color[1] + 20
-                        and bg_color[2] - 20 <= pixels[j, i][2] <= bg_color[2] + 20):
+                            and bg_color[1] - 20 <= pixels[j, i][1] <= bg_color[1] + 20
+                            and bg_color[2] - 20 <= pixels[j, i][2] <= bg_color[2] + 20):
                         # change background to white
                         pixels[j, i] = (255, 255, 255)
                         bg_hori_ratio += 1
@@ -245,7 +251,7 @@ class PDFPlumberParserPlus(PDFPlumberParser):
             if bg_ratio / (image_tuple[0].size[0] * image_tuple[0].size[1]) < 0.7:
                 # if background is not white enough, skip
                 continue
-            # check if image is colored
+
             rgb = image_tuple[0].split()
             if ImageChops.difference(rgb[0], rgb[1]).getextrema()[1] > 30:
                 continue
@@ -258,7 +264,7 @@ class PDFPlumberParserPlus(PDFPlumberParser):
         image_dict_list = []
         for image_tuple in filted_images:
             bg_horizontal_ratio = image_tuple[2]
-            # get every non-1 (= non-background) segment and its pos
+            # get every non-one (= background) segment and its pos
             segment_dict_list = []
             is_bg = True
             for i in range(len(bg_horizontal_ratio)):
@@ -313,16 +319,16 @@ class PDFPlumberParserPlus(PDFPlumberParser):
             print(blank_dict_list)
             print(crop_height_list)
             """
-            # crop the image and append results in dict form
             accum_height = 0
             pos_per_pixel = (image_tuple[1]['bottom'] - image_tuple[1]['top']) / image_tuple[0].height
             for crop_height in crop_height_list:
-                image_dict_list.append({'image': image_tuple[0].crop((0, accum_height, image_tuple[0].width, crop_height)),
-                                        'x0': image_tuple[1]['x0'],
-                                        'x1': image_tuple[1]['x1'],
-                                        'top': image_tuple[1]['top'] + accum_height * pos_per_pixel,
-                                        'doctop': image_tuple[1]['doctop'] + accum_height * pos_per_pixel,
-                                        'bottom': image_tuple[1]['bottom'] - (image_tuple[0].height - crop_height) * pos_per_pixel, })
+                image_dict_list.append(
+                    {'image': image_tuple[0].crop((0, accum_height, image_tuple[0].width, crop_height)),
+                     'x0': image_tuple[1]['x0'],
+                     'x1': image_tuple[1]['x1'],
+                     'top': image_tuple[1]['top'] + accum_height * pos_per_pixel,
+                     'doctop': image_tuple[1]['doctop'] + accum_height * pos_per_pixel,
+                     'bottom': image_tuple[1]['bottom'] - (image_tuple[0].height - crop_height) * pos_per_pixel, })
                 accum_height = crop_height
 
         converted_images = []
