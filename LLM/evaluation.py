2b8f7b7e335c05531fe3d1b46ea8f5e0707e607b
jinmyeongAN <jinmyeong.an@gmail.com>
2023-12-09 23:41:39 +0900

    feat    : set evaluation prompt

---
+++ LLM/evaluation.py
@@ -0,0 +1,12 @@
+evaluation_prompt_template = """
+You are machine learning professor and you should choose better answer about given question.
+
+Question: {question}
+
+Answer 1: {answer_1}
+
+Answer 2: {answer_2}
+
+Which one is better for the question? No explanation Just choose one.
+"""
+
